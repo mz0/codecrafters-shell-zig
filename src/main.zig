f93962046a7d6ecd639d51c9705db55f01532d60
mz0 <mz0@outlook.com>
2026-01-26 13:27:05 +0400

    steps 1,2: "command not found" loop

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,18 @@ const std = @import("std");
 
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
+var stdin_buffer: [4096]u8 = undefined;
+var stdin_reader = std.fs.File.stdin().readerStreaming(&stdin_buffer);
+const stdin = &stdin_reader.interface;
 
 pub fn main() !void {
-    try stdout.print("$ ", .{});
+    while (true) {
+        try stdout.print("$ ", .{});
+        if (try stdin.takeDelimiter('\n')) |line| {
+            const trimmed = std.mem.trim(u8, line, " \r");
+            try stdout.print("{s}: command not found\n", .{trimmed});
+        } else {
+            break; // ^D - EOF reached - Exit
+        }
+    }
 }
