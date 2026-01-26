2c4201ed09663305dbddc2b181072e86a646fb76
mz0 <mz0@outlook.com>
2026-01-27 01:23:50 +0400

    step 3: "exit [smth]" command

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,9 @@ pub fn main() !void {
         try stdout.print("$ ", .{});
         if (try stdin.takeDelimiter('\n')) |line| {
             const trimmed = std.mem.trim(u8, line, " \r");
+            var commands = std.mem.splitSequence(u8, trimmed, " ");
+            const command = commands.first();
+            if (std.mem.eql(u8, command, "exit")) { break; }
             try stdout.print("{s}: command not found\n", .{trimmed});
         } else {
             break; // ^D - EOF reached - Exit
